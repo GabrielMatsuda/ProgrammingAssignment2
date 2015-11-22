45c7dd47fca0cf28679247f1f1695ec5ec87bee8
Gabriel Matsuda <gabriel.matsuda@gmail.com>
2015-11-22 10:58:35 -0200

    Add cache matrix.R
    The functions presented in this file are used to calculate the inverse
    of an invertible matrix using cache to reduce time-consuming
    computations.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions presented below are used to calculate the inverse of an invertible matrix using cache
+## to reduce time-consuming computations. 
 
-## Write a short comment describing this function
+## This function creates a list of auxiliary functions used to get and set 
+## the inverse matrix of an invertible matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setSolvedMatrix <- function(solve) m <<- solve
+  getSolvedMatrix <- function() m
+  
+  list( set = set, 
+        get = get,
+        setSolvedMatrix = setSolvedMatrix,
+        getSolvedMatrix = getSolvedMatrix)
 }
 
 
-## Write a short comment describing this function
+## This function is used to calculate the inverse of an invertible matrix using R's solve function.
+## If the inverse of the matrix hasn't already been calculated, then the inverse is calculated and stored
+## in cache. Else, the inverse is restored from cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  m <- x$getSolvedMatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setSolvedMatrix(m)
+  m
 }
